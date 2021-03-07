1a0e9e81d7d3777dc392df54b9cff51e61ee01ed
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-03-07 22:58:37 +0900

    Level1 CountPnY

--- src/level1/CountPny.java
+++ src/level1/CountPny.java
@@ -1,5 +1,37 @@
 package level1;
 
 public class CountPny {
+	/**
+	 * 프로그래머스 Level 1 문자열 내 P와 Y의 개수
+	 * https://programmers.co.kr/learn/courses/30/lessons/12916?language=java
+	 * 점수 : 1005
+	 */
 
+	public static boolean solution(String s) {
+		// 1. P,Y 개수가 같으면 트루 아니면 펄스
+		// 2. p,y가 하나도 없으면 트루
+		// 3. 대소문자 구분 x - 로우, 어퍼 클래스
+		s = s.toLowerCase();
+		int numP = 0;
+		int numY = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'p') {
+				numP++;
+			} else if (s.charAt(i) == 'y') {
+				numY++;
+			}
+		}
+
+		if (numP == 0 && numY == 0) {
+			return true;
+		} else if (numP == numY) {
+			return true;
+		} 
+		return false;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution("ofof"));
+
+	}
 }
