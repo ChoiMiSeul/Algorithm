43ef715213a4d1b08f8c26855eae190b98721304
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-03-13 02:32:03 +0900

    Update NoFinishPlayer.java

--- src/level1/NoFinishPlayer.java
+++ src/level1/NoFinishPlayer.java
@@ -4,7 +4,7 @@ public class NoFinishPlayer {
 	/**
 	 * 프로그래머스 Level 1 완주하지 못한 선수 
 	 * https://programmers.co.kr/learn/courses/30/lessons/42576
-	 * 점수 :
+	 * 점수 : +8
 	 * 날짜 : 210312 12:40~02:00
 	 */
 	public static String solution(String[] participant, String[] completion) {
