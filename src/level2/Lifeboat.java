6759a995bd6e3e77b81548fb4fb45225920a87e1
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-06-24 00:07:01 +0900

    update answer

--- src/level2/Lifeboat.java
+++ src/level2/Lifeboat.java
@@ -5,22 +5,21 @@ import java.util.Arrays;
 public class Lifeboat {
 	public static int solution(int[] people, int limit) {
         int count = 0;
-        int sum = 0;
+        int min = 0;
         
         Arrays.sort(people);
-        for(int i=0; i<people.length; i++) {
-        	sum += people[i];
-        	if(sum + people[i+1] > limit) {
-        		count++;
-        		sum = 0;
+        for(int max = people.length-1; min<=max; max--) {
+        	if(people[min] + people[max] <= limit) {
+        		min++;
         	}
+        	count++;
         }
         
 		return count;
     }
 	
 	public static void main(String[] args) {
-		int[] people = {70,50,80,50};
+		int[] people = {70,50,80};
 		System.out.println(solution(people, 100));
 	}
 }
