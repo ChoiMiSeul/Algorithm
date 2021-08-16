4b68978e3dc88e0b7f3c663bdc2b83bc63a3a75d
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-08-16 17:24:42 +0900

    210815

--- src/level2/FindPrimeNumber.java
+++ src/level2/FindPrimeNumber.java
@@ -1,21 +1,83 @@
 package level2;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 프로그래머스 Level2 소수 찾기
+ * https://programmers.co.kr/learn/courses/30/lessons/42839
+ * 점수 : +4
+ * 날짜 : 210815
+ */
 public class FindPrimeNumber {
-	public static int solution(String numbers) {
-        int answer = 0;
-        int[] num = new int[numbers.length()];
+	Set<Integer> set;
+	boolean[] visited;
+
+	public int solution(String numbers) {
+		set = new HashSet<>(); // 조합담기
+		visited = new boolean[numbers.length()]; // 이전값 선택여부
+
+		String[] strToInt = numbers.split("");
+		int[] intArr = Arrays.asList(strToInt).stream().mapToInt(Integer::parseInt).toArray();
+		combine(intArr, "");
+
+		int count = 0;
+		Iterator<Integer> da = set.iterator();
+		while (da.hasNext()) {
+			int number = da.next();
+			if (primeNum(number)) {
+				count++;
+			}
+		}
+		
+//		Set<Integer> s = set.stream().filter(i -> primeNum(i)).collect(Collectors.toSet());
+//		System.out.println(s.size());
+		
+		return count;
+	}
+
+	public void combine(int[] num, String combinedNum) {
+		// 선택된 건 선택x
+		// 선택 여부 판단 boolean visited
+
+		// 재귀 1.종료조건 2.로직
+		if (!combinedNum.equals("")) {
+			set.add(Integer.parseInt(combinedNum));
+		}
+
+		for (int i = 0; i < num.length; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				combine(num, combinedNum + num[i]);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public boolean primeNum(int n) {
+		if(n < 2) {
+			return false;
+		}
+		
+		if(n == 2) {
+			return true;
+		}
         
-        //numbers의 숫자 각각 int형으로 바꿔서 배열에 저장
-        for(int i=0; i<numbers.length(); i++) {
-        	num[i] = Character.getNumericValue(numbers.charAt(i));
-        }
-        
-        
-        
-        return answer;
-    }
-	
+		for(int i = 2; i < n; i++) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(solution("17"));
+
+		FindPrimeNumber a = new FindPrimeNumber();
+		System.out.println(a.solution("011"));
 	}
 }
